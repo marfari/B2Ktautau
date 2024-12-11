318461e741fbf316a0cc418b382160570f6edef6
Feliciano Faria Maria Carolina <felician@lphesrv1.epfl.ch>
2024-12-11 21:00:56 +0100

    update

--- createDataset.C
+++ createDataset.C
@@ -32,9 +32,9 @@ TString variables_DDK[] = {"mass",
                     };
 
 // D0bar D+s variables
-TString variables_DDs[] = {"mass",
+TString variables_DDs[] = {"mass", "BDT1", "BDT2", 
                     "Bp_FD_OWNPV", "D0bar_FD_ORIVX", "Dsp_FD_ORIVX", 
-                    "Bp_PT", "Bp_ETA", "D0bar_PT", "Dsp_PT",
+                    "Bp_P", "Bp_PT", "Bp_ETA", "D0bar_PT", "Dsp_PT",
                     "D0bar_PX", "D0bar_PY", "D0bar_PZ", "Dsp_PX", "Dsp_PY", "Dsp_PZ", "Bp_PX", "Bp_PY", "Bp_PZ",
                     "D0bar_K_IPCHI2_OWNPV", "D0bar_pi_IPCHI2_OWNPV", "Dsp_K1_IPCHI2_OWNPV", "Dsp_K2_IPCHI2_OWNPV", "Dsp_pi_IPCHI2_OWNPV",
                     "Bp_ENDVERTEX_X", "Bp_ENDVERTEX_Y", "Bp_ENDVERTEX_Z", "Bp_ENDVERTEX_CHI2",
@@ -43,13 +43,28 @@ TString variables_DDs[] = {"mass",
                     "D0bar_M", "Dsp_M",
                     "D0bar_K_ProbNNk", "D0bar_pi_ProbNNk", "Dsp_K1_ProbNNk", "Dsp_K2_ProbNNk", "Dsp_pi_ProbNNk",
                     "D0bar_K_ProbNNpi", "D0bar_pi_ProbNNpi", "Dsp_K1_ProbNNpi", "Dsp_K2_ProbNNpi", "Dsp_pi_ProbNNpi",
-                    "D0bar_K_TRGHOSTPROB", "D0bar_pi_TRGHOSTPROB", "Dsp_K1_TRGHOSTPROB", "Dsp_K2_TRGHOSTPROB", "Dsp_pi_TRGHOSTPROB",
-                    "Bp_BSTAUTAUTAUISOBDTFIRSTVALUETAUP", "Bp_BSTAUTAUTAUISOBDTSECONDVALUETAUP", "Bp_BSTAUTAUTAUISOBDTTHIRDVALUETAUP", "Bp_BSTAUTAUTAUISOBDTFIRSTVALUETAUM", "Bp_BSTAUTAUTAUISOBDTSECONDVALUETAUM", "Bp_BSTAUTAUTAUISOBDTTHIRDVALUETAUM",
-                    "Bp_BSTAUTAUTAUISOFIRSTVALUETAUP", "Bp_BSTAUTAUTAUISOSECONDVALUETAUP", "Bp_BSTAUTAUTAUISOFIRSTVALUETAUM", "Bp_BSTAUTAUTAUISOSECONDVALUETAUM",
-                    "Bp_BSTAUTAUTRKISOBDTFIRSTVALUETAUPPIM", "Bp_BSTAUTAUTRKISOBDTSECONDVALUETAUPPIM", "Bp_BSTAUTAUTRKISOBDTTHIRDVALUETAUPPIM", "Bp_BSTAUTAUTRKISOBDTFIRSTVALUETAUPPIP1", "Bp_BSTAUTAUTRKISOBDTSECONDVALUETAUPPIP1", "Bp_BSTAUTAUTRKISOBDTTHIRDVALUETAUPPIP1", "Bp_BSTAUTAUTRKISOBDTFIRSTVALUETAUPPIP2", "Bp_BSTAUTAUTRKISOBDTSECONDVALUETAUPPIP2", "Bp_BSTAUTAUTRKISOBDTTHIRDVALUETAUPPIP2", "Bp_BSTAUTAUTRKISOBDTFIRSTVALUETAUMPIP", "Bp_BSTAUTAUTRKISOBDTSECONDVALUETAUMPIP", "Bp_BSTAUTAUTRKISOBDTTHIRDVALUETAUMPIP", "Bp_BSTAUTAUTRKISOBDTFIRSTVALUETAUMPIM1", "Bp_BSTAUTAUTRKISOBDTSECONDVALUETAUMPIM1", "Bp_BSTAUTAUTRKISOBDTTHIRDVALUETAUMPIM1", "Bp_BSTAUTAUTRKISOBDTFIRSTVALUETAUMPIM2", "Bp_BSTAUTAUTRKISOBDTSECONDVALUETAUMPIM2", "Bp_BSTAUTAUTRKISOBDTTHIRDVALUETAUMPIM2",
-                    "Bp_BSTAUTAUTRKISOFIRSTVALUETAUPPIM", "Bp_BSTAUTAUTRKISOFIRSTVALUETAUPPIP1", "Bp_BSTAUTAUTRKISOFIRSTVALUETAUPPIP2", "Bp_BSTAUTAUTRKISOFIRSTVALUETAUMPIP", "Bp_BSTAUTAUTRKISOFIRSTVALUETAUMPIM1", "Bp_BSTAUTAUTRKISOFIRSTVALUETAUMPIM2",
-                    "nPVs", "nTracks", "nLongTracks", "nDownstreamTracks", "nUpstreamTracks", "nVeloTracks", "nTTracks", "nVeloClusters", "nITClusters", "nSPDHits"
+                    "Bp_VTXISONUMVTX_B", "Bp_VTXISODCHI2ONETRACK_B", "Bp_VTXISODCHI2MASSONETRACK_B", "Bp_VTXISODCHI2TWOTRACK_B", "Bp_VTXISODCHI2MASSTWOTRACK_B",
+                    "Bp_VTXISONUMVTX_D0bar", "Bp_VTXISODCHI2ONETRACK_D0bar", "Bp_VTXISODCHI2MASSONETRACK_D0bar", "Bp_VTXISODCHI2TWOTRACK_D0bar", "Bp_VTXISODCHI2MASSTWOTRACK_D0bar",
+                    "Bp_VTXISONUMVTX_Dsp", "Bp_VTXISODCHI2ONETRACK_Dsp", "Bp_VTXISODCHI2MASSONETRACK_Dsp", "Bp_VTXISODCHI2TWOTRACK_Dsp", "Bp_VTXISODCHI2MASSTWOTRACK_Dsp",
+                    "Bp_CC_05_MULT_B", "Bp_CC_05_SPT_B", "Bp_CC_05_VPT_B", "Bp_CC_05_PX_B", "Bp_CC_05_PY_B", "Bp_CC_05_PZ_B", "Bp_CC_05_PASYM_B", "Bp_CC_05_PTASYM_B", "Bp_CC_05_PXASYM_B", "Bp_CC_05_PYASYM_B", "Bp_CC_05_PZASYM_B", "Bp_CC_05_DELTAETA_B", "Bp_CC_05_DELTAPHI_B", "Bp_CC_05_IT_B", "Bp_CC_05_MAXPT_Q_B", "Bp_CC_05_MAXPT_PT_B", "Bp_CC_05_MAXPT_PX_B", "Bp_CC_05_MAXPT_PY_B", "Bp_CC_05_MAXPT_PZ_B", "Bp_CC_05_MAXPT_PE_B", 
+                    "Bp_NC_05_MULT_B", "Bp_NC_05_SPT_B", "Bp_NC_05_VPT_B", "Bp_NC_05_PX_B", "Bp_NC_05_PY_B", "Bp_NC_05_PZ_B", "Bp_NC_05_PASYM_B", "Bp_NC_05_PTASYM_B", "Bp_NC_05_PXASYM_B", "Bp_NC_05_PYASYM_B", "Bp_NC_05_PZASYM_B", "Bp_NC_05_DELTAETA_B", "Bp_NC_05_DELTAPHI_B", "Bp_NC_05_IT_B", "Bp_NC_05_MAXPT_PT_B", "Bp_NC_05_MAXPT_PX_B", "Bp_NC_05_MAXPT_PY_B", "Bp_NC_05_MAXPT_PZ_B", "Bp_CCNC_05_IT_B",
+                    "Bp_CC_05_MULT_D0bar", "Bp_CC_05_SPT_D0bar", "Bp_CC_05_VPT_D0bar", "Bp_CC_05_PX_D0bar", "Bp_CC_05_PY_D0bar", "Bp_CC_05_PZ_D0bar", "Bp_CC_05_PASYM_D0bar", "Bp_CC_05_PTASYM_D0bar", "Bp_CC_05_PXASYM_D0bar", "Bp_CC_05_PYASYM_D0bar", "Bp_CC_05_PZASYM_D0bar", "Bp_CC_05_DELTAETA_D0bar", "Bp_CC_05_DELTAPHI_D0bar", "Bp_CC_05_IT_D0bar", "Bp_CC_05_MAXPT_Q_D0bar", "Bp_CC_05_MAXPT_PT_D0bar", "Bp_CC_05_MAXPT_PX_D0bar", "Bp_CC_05_MAXPT_PY_D0bar", "Bp_CC_05_MAXPT_PZ_D0bar", "Bp_CC_05_MAXPT_PE_D0bar", 
+                    "Bp_NC_05_MULT_D0bar", "Bp_NC_05_SPT_D0bar", "Bp_NC_05_VPT_D0bar", "Bp_NC_05_PX_D0bar", "Bp_NC_05_PY_D0bar", "Bp_NC_05_PZ_D0bar", "Bp_NC_05_PASYM_D0bar", "Bp_NC_05_PTASYM_D0bar", "Bp_NC_05_PXASYM_D0bar", "Bp_NC_05_PYASYM_D0bar", "Bp_NC_05_PZASYM_D0bar", "Bp_NC_05_DELTAETA_D0bar", "Bp_NC_05_DELTAPHI_D0bar", "Bp_NC_05_IT_D0bar", "Bp_NC_05_MAXPT_PT_D0bar", "Bp_NC_05_MAXPT_PX_D0bar", "Bp_NC_05_MAXPT_PY_D0bar", "Bp_NC_05_MAXPT_PZ_D0bar", "Bp_CCNC_05_IT_D0bar",
+                    "Bp_CC_05_MULT_Dsp", "Bp_CC_05_SPT_Dsp", "Bp_CC_05_VPT_Dsp", "Bp_CC_05_PX_Dsp", "Bp_CC_05_PY_Dsp", "Bp_CC_05_PZ_Dsp", "Bp_CC_05_PASYM_Dsp", "Bp_CC_05_PTASYM_Dsp", "Bp_CC_05_PXASYM_Dsp", "Bp_CC_05_PYASYM_Dsp", "Bp_CC_05_PZASYM_Dsp", "Bp_CC_05_DELTAETA_Dsp", "Bp_CC_05_DELTAPHI_Dsp", "Bp_CC_05_IT_Dsp", "Bp_CC_05_MAXPT_Q_Dsp", "Bp_CC_05_MAXPT_PT_Dsp", "Bp_CC_05_MAXPT_PX_Dsp", "Bp_CC_05_MAXPT_PY_Dsp", "Bp_CC_05_MAXPT_PZ_Dsp", "Bp_CC_05_MAXPT_PE_Dsp", 
+                    "Bp_NC_05_MULT_Dsp", "Bp_NC_05_SPT_Dsp", "Bp_NC_05_VPT_Dsp", "Bp_NC_05_PX_Dsp", "Bp_NC_05_PY_Dsp", "Bp_NC_05_PZ_Dsp", "Bp_NC_05_PASYM_Dsp", "Bp_NC_05_PTASYM_Dsp", "Bp_NC_05_PXASYM_Dsp", "Bp_NC_05_PYASYM_Dsp", "Bp_NC_05_PZASYM_Dsp", "Bp_NC_05_DELTAETA_Dsp", "Bp_NC_05_DELTAPHI_Dsp", "Bp_NC_05_IT_Dsp", "Bp_NC_05_MAXPT_PT_Dsp", "Bp_NC_05_MAXPT_PX_Dsp", "Bp_NC_05_MAXPT_PY_Dsp", "Bp_NC_05_MAXPT_PZ_Dsp", "Bp_CCNC_05_IT_Dsp",
+                    "Bp_VTXISOBDTHARDFIRSTVALUE_B", "Bp_VTXISOBDTHARDSECONDVALUE_B", "Bp_VTXISOBDTHARDTHIRDVALUE_B",
+                    "Bp_VTXISOBDTHARDFIRSTVALUE_D0bar", "Bp_VTXISOBDTHARDSECONDVALUE_D0bar", "Bp_VTXISOBDTHARDTHIRDVALUE_D0bar",
+                    "Bp_VTXISOBDTHARDFIRSTVALUE_Dsp", "Bp_VTXISOBDTHARDSECONDVALUE_Dsp", "Bp_VTXISOBDTHARDTHIRDVALUE_Dsp",
+                    "Bp_TRKISOBDTFIRSTVALUE_D0bar_K", "Bp_TRKISOBDTSECONDVALUE_D0bar_K", "Bp_TRKISOBDTTHIRDVALUE_D0bar_K",
+                    "Bp_TRKISOBDTFIRSTVALUE_D0bar_pi", "Bp_TRKISOBDTSECONDVALUE_D0bar_pi", "Bp_TRKISOBDTTHIRDVALUE_D0bar_pi",
+                    "Bp_TRKISOBDTFIRSTVALUE_Dsp_K1", "Bp_TRKISOBDTSECONDVALUE_Dsp_K1", "Bp_TRKISOBDTTHIRDVALUE_Dsp_K1",
+                    "Bp_TRKISOBDTFIRSTVALUE_Dsp_K2", "Bp_TRKISOBDTSECONDVALUE_Dsp_K2", "Bp_TRKISOBDTTHIRDVALUE_Dsp_K2",
+                    "Bp_TRKISOBDTFIRSTVALUE_Dsp_pi", "Bp_TRKISOBDTSECONDVALUE_Dsp_pi", "Bp_TRKISOBDTTHIRDVALUE_Dsp_pi",
+                    "Bp_Bstautau_ISOBDTFIRSTVALUE_taup", "Bp_Bstautau_ISOBDTSECONDVALUE_taup", "Bp_Bstautau_ISOBDTTHIRDVALUE_taup", "Bp_Bstautau_ISOBDTFIRSTVALUE_taum", "Bp_Bstautau_ISOBDTSECONDVALUE_taum", "Bp_Bstautau_ISOBDTTHIRDVALUE_taum",
+                    "nPVs", "nTracks", "nLongTracks", "nDownstreamTracks", "nUpstreamTracks", "nVeloTracks", "nTTracks", "nVeloClusters", "nITClusters", "nSPDHits",
                     };
+
+TString variables_DDs_trigger[] = {"mass", "Bp_L0HadronDecision_TOS", "Bp_L0HadronDecision_TIS", "Bp_L0MuonDecision_TIS", "Bp_L0ElectronDecision_TIS", "Bp_L0PhotonDecision_TIS", "Bp_Hlt1TrackMVADecision_TOS", "Bp_Hlt1TwoTrackMVADecision_TOS", "Bp_Hlt2Topo2BodyDecision_TOS", "Bp_Hlt2Topo3BodyDecision_TOS", "Bp_Hlt2Topo4BodyDecision_TOS"};
 
 // D0D0K
 TString variables_D0D0K[] = {"mass",
@@ -81,6 +96,7 @@ void createDataset(int year, int species, TString FILES)
         isKtautauMC = true;
     }
 
+    cout << "Opening .root files" << endl;
     TFileCollection *fc;
     if(isKtautauMC)
     {
@@ -93,12 +109,35 @@ void createDataset(int year, int species, TString FILES)
     TChain* t = new TChain("DecayTree");
     t->AddFileInfoList((TCollection*)fc->GetList());
 
-    // Require events to pass DTF
-    TTree* t_cut = (TTree*)t->CopyTree("");
+    TChain* t1;
+    if((species == 7) || (species == 8))
+    {
+        TFileCollection *fc1 = new TFileCollection("fc1", "fc1", Form("/panfs/felician/B2Ktautau/workflow/sklearn_response/201%i/Species_%i/bdt_output.txt",year,species));
+        t1 = new TChain("XGBoost/DecayTree");
+        t1->AddFileInfoList((TCollection*)fc1->GetList());
+    }
 
+    cout << "Creating TTree" << endl;
+    TTree* t_cut;
+    if(species == 85)
+    {
+        t_cut = (TTree*)t->CopyTree("(Bp_dtf_M[0] > 5235) && (Bp_dtf_M[0] < 5355)");
+    }
+    else
+    {
+        t_cut = (TTree*)t->CopyTree("");
+    }
+    
     // Flatten the DTF B+ mass (vector branch)
-    Double_t theMass;
+    Double_t theMass, theBDT1, theBDT2;
     TBranch* bmass = t_cut->Branch("mass", &theMass);
+    TBranch* bdt1; 
+    TBranch* bdt2; 
+    if((species == 7) || (species == 8))
+    {
+        bdt1 = t_cut->Branch("BDT1", &theBDT1);
+        bdt2 = t_cut->Branch("BDT2", &theBDT2);
+    }
 
     Float_t DTF_mass[100];
     Double_t fit_mass;
@@ -110,7 +149,15 @@ void createDataset(int year, int species, TString FILES)
     {
         t_cut->SetBranchAddress("Bp_dtf_M", DTF_mass);
     }
+    
+    Double_t BDT1, BDT2;
+    if((species == 7) || (species == 8))
+    {
+        t1->SetBranchAddress("BDT1", &BDT1);
+        t1->SetBranchAddress("BDT2", &BDT2);
+    }
 
+    cout << "Filling mass branch" << endl;
     for(int i = 0; i < t_cut->GetEntries(); i++)
     {
         t_cut->GetEntry(i);
@@ -125,10 +172,23 @@ void createDataset(int year, int species, TString FILES)
         bmass->Fill();
     }
 
+    if((species == 7) || (species == 8))
+    {
+        for(int i = 0; i < t1->GetEntries(); i++)
+        {
+            t1->GetEntry(i);
+            theBDT1 = BDT1;
+            theBDT2 = BDT2;
+            bdt1->Fill();
+            bdt2->Fill();
+        }
+    }
+
     // Create workspace
     RooWorkspace* w = new RooWorkspace("w");
 
     // Setup workspace variables
+    cout << "Adding variables to workspace" << endl;
     Int_t n_vars;
     RooArgList arg_list("arg_list");
     if(isKtautauMC || (species == 2) || (species == 3))
@@ -149,13 +209,43 @@ void createDataset(int year, int species, TString FILES)
             arg_list.add(*w->var(variables_DDK[i]));
         }   
     }
-    else if((species == 7) || (species == 8))
+    else if(species == 7)
+    {
+
+        TString variables_DDs_1[] =  {"D0bar_K_ProbNNk_pidgen_default", "D0bar_pi_ProbNNk_pidgen_default", "Dsp_K1_ProbNNk_pidgen_default", "Dsp_K2_ProbNNk_pidgen_default", "Dsp_pi_ProbNNk_pidgen_default",
+                    "D0bar_K_ProbNNpi_pidgen_default", "D0bar_pi_ProbNNpi_pidgen_default", "Dsp_K1_ProbNNpi_pidgen_default", "Dsp_K2_ProbNNpi_pidgen_default", "Dsp_pi_ProbNNpi_pidgen_default"};
+
+        n_vars = sizeof(variables_DDs)/sizeof(variables_DDs[0]);
+        store_vars_in_workspace(w, t_cut, n_vars, variables_DDs, species, isKtautauMC);
+        for(int i = 0; i < n_vars; i++)
+        {
+            arg_list.add(*w->var(variables_DDs[i]));
+        }
+
+        Int_t n_vars_1 = sizeof(variables_DDs_1)/sizeof(variables_DDs_1[0]);
+        store_vars_in_workspace(w, t_cut, n_vars_1, variables_DDs_1, species, isKtautauMC);
+        for(int i = 0; i < n_vars_1; i++)
+        {
+            arg_list.add(*w->var(variables_DDs_1[i]));
+        }
+
+    }
+    else if(species == 8)
     {
         n_vars = sizeof(variables_DDs)/sizeof(variables_DDs[0]);
         store_vars_in_workspace(w, t_cut, n_vars, variables_DDs, species, isKtautauMC);
         for(int i = 0; i < n_vars; i++)
         {
             arg_list.add(*w->var(variables_DDs[i]));
+        }
+    }
+    else if(species == 85)
+    {
+        Int_t n_vars_1 = sizeof(variables_DDs_trigger)/sizeof(variables_DDs_trigger[0]);
+        store_vars_in_workspace(w, t_cut, n_vars_1, variables_DDs_trigger, species, isKtautauMC);
+        for(int i = 0; i < n_vars_1; i++)
+        {
+            arg_list.add(*w->var(variables_DDs_trigger[i]));
         }
     }
     else if((species == 9) || (species == 0) || (species == -1))
@@ -169,9 +259,9 @@ void createDataset(int year, int species, TString FILES)
     }
 
     // Add variables to a RooDataSet
-    RooDataSet* data;
-    data = new RooDataSet("data","data",t_cut,arg_list);
-    
+    cout << "Creating RooDataSet from TTree" << endl;
+    RooDataSet* data = new RooDataSet("data","data",t_cut,arg_list);
+
     w->import(*data);
     w->Print();
 
@@ -192,7 +282,7 @@ void store_vars_in_workspace(RooWorkspace* w, TTree* t, Int_t n_vars, TString va
             {
                 vars.push_back( new RooRealVar(variables[i], variables[i], 5180, 5600 ) ); 
             }
-            else if((species == 7) || (species == 71) || (species == 8) || (species == 81)) // D0bar D+s
+            else if((species == 7) || (species == 71) || (species == 8) || (species == 81) || (species == 85)) // D0bar D+s
             {
                 vars.push_back( new RooRealVar(variables[i], variables[i], 5235, 5355 ) ); 
             }
--- create_pre_sel_tree.C
+++ create_pre_sel_tree.C
@@ -73,15 +73,15 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
     {
         FILES = Form("Files_on_grid/data_201%i.txt",year);
     }
-    else if((species == 5) || (species == 6)) // DDK data
+    else if((species == 5) || (species == 6)) // D+D-K data
     {
         FILES = Form("Files_on_grid/data_DDK_201%i.txt",year);
     }
-    else if( (species == 7) || (species == 71) || (species == 73) || (species == 74) )
+    else if( (species == 7) || (species == 71) || (species == 74) )
     {
         FILES = Form("/panfs/felician/B2Ktautau/workflow/PIDCalib/201%i/Species_7/pid_corr.txt",year);
     }
-    else if((species == 8) || (species == 81) || (species == 82) || (species == 83) || (species == 84) )
+    else if((species == 8) || (species == 81) || (species == 82) || (species == 83) || (species == 84) || (species == 85))
     {
         FILES = Form("Files_on_grid/data_D0Dps_201%i.txt",year);
     }
@@ -95,19 +95,19 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
     }
     else if(isBuDDKp_cocktail)
     {
-        FILES = Form("/panfs/felician/BuDDKp_cocktail/root_files_201%i.txt",year);
+        FILES = Form("Files_on_grid/MC_201%i_BuDDKp_cocktail.txt",year);
     }
     else if(isBdDDKp_cocktail)
     {
-        FILES = Form("/panfs/felician/BdDDKp_cocktail/root_files_201%i.txt",year);   
+        FILES = Form("Files_on_grid/MC_201%i_BdDDKp_cocktail.txt",year);   
     }
     else if(isBsDDKp_cocktail)
     {
-        FILES = Form("/panfs/felician/BsDDKp_cocktail/root_files_201%i.txt",year);  
+        FILES = Form("Files_on_grid/MC_201%i_BsDDKp_cocktail.txt",year);  
     }
     else if(isBuDDK0_cocktail)
     {
-        FILES = Form("/panfs/felician/BuDDK0_cocktail/root_files_201%i.txt",year);  
+        FILES = Form("Files_on_grid/MC_201%i_BuDDK0_cocktail.txt",year);  
     }
     else if(isBdDDK0_cocktail)
     {
@@ -115,7 +115,7 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
     }
     else if(isBuDD_cocktail)
     {
-        FILES = Form("/panfs/felician/BuDD_cocktail/root_files_201%i.txt",year);
+        FILES = Form("Files_on_grid/MC_201%i_BuDD_cocktail.txt",year);
     }
     else if(isBdDD_cocktail)
     {
@@ -142,7 +142,7 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
     {
         truthMatch = "(abs(Kp_TRUEID) == 321) && (abs(Dp_K_TRUEID) == 321) && (abs(Dp_pi1_TRUEID) == 211) && (abs(Dp_pi2_TRUEID) == 211) && (abs(Dm_K_TRUEID) == 321) && (abs(Dm_pi1_TRUEID) == 211) && (abs(Dm_pi2_TRUEID) == 211) && (abs(Dp_TRUEID) == 411) && (abs(Dm_TRUEID) == 411) && (abs(Bp_TRUEID) == 521)";
     }
-    else if((species == 7) || (species == 73) || (species == 74) ) // D0D+s MC
+    else if((species == 7) || (species == 74) ) // D0D+s MC
     {
         truthMatch = "(abs(D0bar_K_TRUEID) == 321) && (abs(Dsp_K1_TRUEID) == 321) && (abs(Dsp_K2_TRUEID) == 321) && (abs(D0bar_pi_TRUEID) == 211) && (abs(Dsp_pi_TRUEID) == 211) && (abs(D0bar_TRUEID) == 421) && (abs(Dsp_TRUEID) == 431) && (abs(Bp_TRUEID) == 521)";
     }
@@ -178,12 +178,12 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
         other_cuts.push_back("(abs(Dp_M-1869.66) < 50) && (abs(Dm_M < 1869.66) < 50)");
         other_cuts.push_back("(Bp_FD_OWNPV > 2)");
     }
-    if((species == 7) || (species == 8) || (species == 73)) // D0barD+s
+    if((species == 7) || (species == 8) ) // D0barD+s
     { 
         other_cuts.push_back("Bp_FD_OWNPV < 80");
         other_cuts.push_back("(D0bar_M > 1820) && (D0bar_M < 1910)");
         other_cuts.push_back("(Dsp_M > 1930) && (Dsp_M < 2000)");
-        if((species == 7) || (species == 73))
+        if(species == 7)
         {
             other_cuts.push_back("D0bar_K_ProbNNk_pidgen_default > 0.55");
             other_cuts.push_back("Dsp_K1_ProbNNk_pidgen_default > 0.55");
@@ -194,11 +194,6 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
             other_cuts.push_back("D0bar_K_ProbNNk > 0.55");
             other_cuts.push_back("Dsp_K1_ProbNNk > 0.55");
             other_cuts.push_back("Dsp_K2_ProbNNk > 0.55");
-        }
-
-        if(species == 73)
-        {
-            other_cuts.push_back("(Bp_dtf_M[0] > 5235) && (Bp_dtf_M[0] < 5355)");
         }
 
     }
@@ -247,7 +242,7 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     TFileCollection* fc = new TFileCollection("fc", "fc", FILES, 1, line);
     TChain* t;
-    if(isKtautauMC || (species == 7) || (species == 73) || (species == 74) ) // Ktautau MC + normalisation channel (PID corrected)
+    if(isKtautauMC || (species == 7) || (species == 74) ) // Ktautau MC + normalisation channel (PID corrected)
     {
         t = new TChain("DecayTree");
     }
@@ -285,7 +280,7 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
     {
         pre_selections = truthMatch+trigger+others;
     }
-    else if((species == 4) || (species == 7) || (species == 9) || (species == 73)) // D+D-K+, D0barD+s, D0D0K MC
+    else if((species == 4) || (species == 7) || (species == 9) ) // D+D-K+, D0barD+s, D0D0K MC
     {
         pre_selections = truthMatch+trigger+others+"(Bp_dtf_status[0]==0)";
     }
@@ -300,6 +295,10 @@ void create_pre_sel_tree(int year, int species, int line, bool createTable)
     else if(species == 84)
     {
         pre_selections = trigger+"(Bp_dtf_status[0]==0)";
+    }
+    else if(species == 85)
+    {
+        pre_selections = "";
     }
     else if(is_cocktailMC)
     {
