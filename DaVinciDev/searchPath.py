543708f32cacf14cf4176829da4c9edc5d2857f1
Feliciano Faria Maria Carolina <felician@lphesrv1.epfl.ch>
2024-12-21 11:41:31 +0100

    remove DaVinciDev

--- DaVinciDev/searchPath.py
+++
@@ -1,9 +0,0 @@
-from LbEnv.ProjectEnv.options import (
-    SearchPath,
-    SearchPathEntry,
-    EnvSearchPathEntry,
-    NightlyPathEntry,
-    LHCbDevPathEntry,
-)
-
-path = SearchPath([EnvSearchPathEntry('CMAKE_PREFIX_PATH', '/cvmfs/lhcb.cern.ch/lib/lhcb:/cvmfs/lhcb.cern.ch/lib/lcg/releases:/cvmfs/lhcb.cern.ch/lib/lcg/app/releases:/cvmfs/lhcb.cern.ch/lib/lcg/external:/cvmfs/lhcb.cern.ch/lib/contrib:/cvmfs/lhcb.cern.ch/lib/var/lib/LbEnv/2399/stable/linux-64/lib/python3.9/site-packages/LbDevTools/data/cmake'), EnvSearchPathEntry('CMTPROJECTPATH', ''), EnvSearchPathEntry('LHCBPROJECTPATH', '')])
