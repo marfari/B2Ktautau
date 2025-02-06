1799a8b4622fcc15fee5754c4224a59dd33182da
Feliciano Faria Maria Carolina <felician@lphesrv1.epfl.ch>
2025-02-06 11:59:41 +0100

    update

--- mass_vetoes_plots.C
+++ mass_vetoes_plots.C
@@ -1,71 +1,29 @@
-void draw_mass_plot(Int_t entry, TChain* t1_2016, TChain* t2_2016, TChain* t3_2016, TString name, TCut mass_vetoe_cuts, Int_t Npar);
+void draw_mass_plot(Int_t entry, TTree* t1, TTree* t2, TTree* t3, TString name, TCut mass_vetoe_cuts, Int_t Npar, Bool_t isWrong);
 Int_t n_choose_k(Int_t n, Int_t k);
 
 void mass_vetoes_plots()
 {
     // Input files
     // 3pi3pi MC
+    TFile* f1 = new TFile("/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/Species_10/post_sel_tree.root");
+    TTree* t1 = (TTree*)f1->Get("DecayTree");  
+
     cout << "3pi3pi MC" << endl;
-    TFileCollection* fc1_2016 = new TFileCollection("fc1_2016", "fc1_2016", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2016/Species_10/post_sel_tree.txt");
-    TFileCollection* fc1_2017 = new TFileCollection("fc1_2017", "fc1_2017", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2017/Species_10/post_sel_tree.txt");
-    TFileCollection* fc1_2018 = new TFileCollection("fc1_2018", "fc1_2018", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2018/Species_10/post_sel_tree.txt");
-
-    TChain* t1_2016 = new TChain("DecayTree");
-    TChain* t1_2017 = new TChain("DecayTree");
-    TChain* t1_2018 = new TChain("DecayTree");
-
-    t1_2016->AddFileInfoList((TCollection*)fc1_2016->GetList());
-    t1_2017->AddFileInfoList((TCollection*)fc1_2017->GetList());
-    t1_2018->AddFileInfoList((TCollection*)fc1_2018->GetList());
-
-    cout << "2016: " << t1_2016->GetEntries() << endl;
-    cout << "2017: " << t1_2017->GetEntries() << endl;
-    cout << "2018: " << t1_2018->GetEntries() << endl;
-
-    t1_2016->Add(t1_2017);
-    t1_2016->Add(t1_2018);
+    cout << t1->GetEntries() << endl;
 
     // RS data
+    TFile* f2 = new TFile("/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/Species_2/post_sel_tree.root");
+    TTree* t2 = (TTree*)f2->Get("DecayTree");  
+
     cout << "RS data" << endl;
-    TFileCollection* fc2_2016 = new TFileCollection("fc2_2016", "fc2_2016", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2016/Species_2/post_sel_tree.txt");
-    TFileCollection* fc2_2017 = new TFileCollection("fc2_2017", "fc2_2017", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2017/Species_2/post_sel_tree.txt");
-    TFileCollection* fc2_2018 = new TFileCollection("fc2_2018", "fc2_2018", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2018/Species_2/post_sel_tree.txt");
-
-    TChain* t2_2016 = new TChain("DecayTree");
-    TChain* t2_2017 = new TChain("DecayTree");
-    TChain* t2_2018 = new TChain("DecayTree");
-
-    t2_2016->AddFileInfoList((TCollection*)fc2_2016->GetList());
-    t2_2017->AddFileInfoList((TCollection*)fc2_2017->GetList());
-    t2_2018->AddFileInfoList((TCollection*)fc2_2018->GetList());
-
-    cout << "2016: " << t2_2016->GetEntries() << endl;
-    cout << "2017: " << t2_2017->GetEntries() << endl;
-    cout << "2018: " << t2_2018->GetEntries() << endl;
-
-    t2_2016->Add(t2_2017);
-    t2_2016->Add(t2_2018);
+    cout << t2->GetEntries() << endl;
 
     // WS data
+    TFile* f3 = new TFile("/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/Species_3/post_sel_tree.root");
+    TTree* t3 = (TTree*)f3->Get("DecayTree");      
+
     cout << "WS data" << endl;
-    TFileCollection* fc3_2016 = new TFileCollection("fc3_2016", "fc3_2016", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2016/Species_3/post_sel_tree.txt");
-    TFileCollection* fc3_2017 = new TFileCollection("fc3_2017", "fc3_2017", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2017/Species_3/post_sel_tree.txt");
-    TFileCollection* fc3_2018 = new TFileCollection("fc3_2018", "fc3_2018", "/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/2018/Species_3/post_sel_tree.txt");
-
-    TChain* t3_2016 = new TChain("DecayTree");
-    TChain* t3_2017 = new TChain("DecayTree");
-    TChain* t3_2018 = new TChain("DecayTree");
-
-    t3_2016->AddFileInfoList((TCollection*)fc3_2016->GetList());
-    t3_2017->AddFileInfoList((TCollection*)fc3_2017->GetList());
-    t3_2018->AddFileInfoList((TCollection*)fc3_2018->GetList());
-
-    cout << "2016: " << t3_2016->GetEntries() << endl;
-    cout << "2017: " << t3_2017->GetEntries() << endl;
-    cout << "2018: " << t3_2018->GetEntries() << endl;
-
-    t3_2016->Add(t3_2017);
-    t3_2016->Add(t3_2018);
+    cout << t3->GetEntries() << endl;
 
     Int_t N2 = n_choose_k(7,2);
     Int_t N3 = n_choose_k(7,3);
@@ -85,11 +43,21 @@ void mass_vetoes_plots()
     Int_t a5 = 0;
     Int_t a6 = 0;
 
-    Double_t M2_MC[N2], M3_MC[N3], M4_MC[N4], M5_MC[N5], M6_MC[N6];
-    Double_t M2_RS[N2], M3_RS[N3], M4_RS[N4], M5_RS[N5], M6_RS[N6];
-    Double_t M2_WS[N2], M3_WS[N3], M4_WS[N4], M5_WS[N5], M6_MC[N6];
+    Int_t b2 = 0;
+    Int_t b3 = 0;
+    Int_t b4 = 0;
+    Int_t b5 = 0;
+    Int_t b6 = 0;
 
-    TCut mass_vetoe_cuts = "";
+    TCut mass_vetoe_cuts = "(TMath::Abs(Bp_M0456 - 1864.84) > 25)";
+
+    Double_t N_sig_num = t1->GetEntries(mass_vetoe_cuts);
+    Double_t N_rs_num = t2->GetEntries(mass_vetoe_cuts);
+    Double_t N_ws_num = t3->GetEntries(mass_vetoe_cuts);
+
+    cout << "Signal eff = " << N_sig_num/(t1->GetEntries())*100 << endl;
+    cout << "RS data eff = " << N_rs_num/(t2->GetEntries())*100 << endl;
+    cout << "WS data eff = " << N_ws_num/(t3->GetEntries())*100 << endl;
 
     for(int i1 = 0; i1 < 7; i1++)
     {
@@ -98,31 +66,72 @@ void mass_vetoes_plots()
             if(i2 > i1)
             {
                 TString name2 = Form("Bp_M%i%i",i1,i2);
-                draw_mass_plot(a2, t1_2016, t2_2016, t3_2016, name2, mass_vetoe_cuts, 2);
+                draw_mass_plot(a2, t1, t2, t3, name2, mass_vetoe_cuts, 2, false);
+                TString name2_massH1 = Form("Bp_M%i%i_massH1",i1,i2);
+                draw_mass_plot(a2, t1, t2, t3, name2_massH1, mass_vetoe_cuts, 2, true);
+                if(i1 == 0)
+                {
+                    TString name2_massH2 = Form("Bp_M%i%i_massH2",i1,i2);
+                    draw_mass_plot(b2, t1, t2, t3, name2_massH2, mass_vetoe_cuts, 2, true);
+                    b2 += 1;
+                }
                 for(int i3 = 1; i3 < 7; i3++)
                 {
                     if(i3 > i2)
                     {
                         TString name3 = Form("Bp_M%i%i%i",i1,i2,i3);
-                        draw_mass_plot(a3, t1_2016, t2_2016, t3_2016, name3, mass_vetoe_cuts, 3);
+                        draw_mass_plot(a3, t1, t2, t3, name3, mass_vetoe_cuts, 3, false);
+                        TString name3_massH1 = Form("Bp_M%i%i%i_massH1",i1,i2,i3);
+                        draw_mass_plot(a3, t1, t2, t3, name3_massH1, mass_vetoe_cuts, 3, true);
+                        if(i1 == 0)
+                        {
+                            TString name3_massH2 = Form("Bp_M%i%i%i_massH2",i1,i2,i3);
+                            draw_mass_plot(b3, t1, t2, t3, name3_massH2, mass_vetoe_cuts, 3, true);
+                            b3 += 1;
+                        }
                         for(int i4 = 1; i4 < 7; i4++)
                         {
                             if(i4 > i3)
                             {
                                 TString name4 = Form("Bp_M%i%i%i%i",i1,i2,i3,i4);
-                                draw_mass_plot(a4, t1_2016, t2_2016, t3_2016, name4, mass_vetoe_cuts, 4);
+                                draw_mass_plot(a4, t1, t2, t3, name4, mass_vetoe_cuts, 4, false);
+                                TString name4_massH1 = Form("Bp_M%i%i%i%i_massH1",i1,i2,i3,i4);
+                                draw_mass_plot(a4, t1, t2, t3, name4_massH1, mass_vetoe_cuts, 4, true);
+                                if(i1 == 0)
+                                {
+                                    TString name4_massH2 = Form("Bp_M%i%i%i%i_massH2",i1,i2,i3,i4);
+                                    draw_mass_plot(b4, t1, t2, t3, name4_massH2, mass_vetoe_cuts, 4, true);
+                                    b4 += 1;
+                                }
                                 for(int i5 = 1; i5 < 7; i5++)
                                 {
                                     if(i5 > i4)
                                     {
                                         TString name5 = Form("Bp_M%i%i%i%i%i",i1,i2,i3,i4,i5);
-                                        draw_mass_plot(a5, t1_2016, t2_2016, t3_2016, name5, mass_vetoe_cuts, 5);
+                                        draw_mass_plot(a5, t1, t2, t3, name5, mass_vetoe_cuts, 5, false);
+                                        TString name5_massH1 = Form("Bp_M%i%i%i%i%i_massH1",i1,i2,i3,i4,i5);
+                                        draw_mass_plot(a5, t1, t2, t3, name5_massH1, mass_vetoe_cuts, 5, true);
+                                        if(i1 ==  0)
+                                        {
+                                            TString name5_massH2 = Form("Bp_M%i%i%i%i%i_massH2",i1,i2,i3,i4,i5);
+                                            draw_mass_plot(b5, t1, t2, t3, name5_massH2, mass_vetoe_cuts, 5, true);
+                                            b5 += 1;
+
+                                        }
                                         for(int i6 = 1; i6 < 7; i6++)
                                         {   
                                             if(i6 > i5)
                                             {
                                                 TString name6 = Form("Bp_M%i%i%i%i%i%i",i1,i2,i3,i4,i5,i6);
-                                                draw_mass_plot(a6, t1_2016, t2_2016, t3_2016, name6, mass_vetoe_cuts, 6);
+                                                draw_mass_plot(a6, t1, t2, t3, name6, mass_vetoe_cuts, 6, false);
+                                                TString name6_massH1 = Form("Bp_M%i%i%i%i%i%i_massH1",i1,i2,i3,i4,i5,i6);
+                                                draw_mass_plot(a6, t1, t2, t3, name6_massH1, mass_vetoe_cuts, 6, true);
+                                                if(i1 == 0)
+                                                {
+                                                    TString name6_massH2 = Form("Bp_M%i%i%i%i%i%i_massH2",i1,i2,i3,i4,i5,i6);
+                                                    draw_mass_plot(b6, t1, t2, t3, name6_massH2, mass_vetoe_cuts, 6, true);
+                                                    b6 += 1;
+                                                }
                                                 a6 += 1;
                                             }
                                         }
@@ -139,76 +148,115 @@ void mass_vetoes_plots()
             }
         }
     }
+    
 }
 
-void draw_mass_plot(Int_t entry, TChain* t1_2016, TChain* t2_2016, TChain* t3_2016, TString name, TCut mass_vetoe_cuts, Int_t Npar)
+void draw_mass_plot(Int_t entry, TTree* t1, TTree* t2, TTree* t3, TString name, TCut mass_vetoe_cuts, Int_t Npar, Bool_t isWrong)
 {
     gStyle->SetOptStat(0);
     // MC vs RS data vs WS data
     TCanvas c;
     c.cd();
 
-    // Double_t x_min_vec[3] = { t1_2016->GetMinimum(name), t2_2016->GetMinimum(name), t3_2016->GetMinimum(name) };
-    // Double_t x_max_vec[3] = { t1_2016->GetMaximum(name), t2_2016->GetMaximum(name), t3_2016->GetMaximum(name) };
+    Int_t nbins = 80;
 
     std::vector<Double_t> x_min_vec;
-    x_min_vec.push_back(t1_2016->GetMinimum(name));
-    x_min_vec.push_back(t2_2016->GetMinimum(name));
-    x_min_vec.push_back(t3_2016->GetMinimum(name));
+    x_min_vec.push_back(t1->GetMinimum(name));
+    x_min_vec.push_back(t2->GetMinimum(name));
+    x_min_vec.push_back(t3->GetMinimum(name));
 
     std::vector<Double_t> x_max_vec;
-    x_max_vec.push_back(t1_2016->GetMaximum(name));
-    x_max_vec.push_back(t2_2016->GetMaximum(name));
-    x_max_vec.push_back(t3_2016->GetMaximum(name));
+    x_max_vec.push_back(t1->GetMaximum(name));
+    x_max_vec.push_back(t2->GetMaximum(name));
+    x_max_vec.push_back(t3->GetMaximum(name));
 
     Double_t x_min = x_min_vec[ std::distance( x_min_vec.begin(), std::min_element(x_min_vec.begin(), x_min_vec.end()) ) ];
     Double_t x_max = x_max_vec[ std::distance( x_max_vec.begin(), std::max_element(x_max_vec.begin(), x_max_vec.end()) ) ];
 
-    TH1D* h_mc = new TH1D(Form("h_%i_mc_%i",Npar,entry), Form("h_%i_mc_%i",Npar,entry), 100, x_min, x_max);
-    TH1D* h_rs_data = new TH1D(Form("h_%i_rs_data_%i",Npar,entry), Form("h_%i_rs_data_%i",Npar,entry), 100, x_min, x_max);
-    TH1D* h_ws_data = new TH1D(Form("h_%i_ws_data_%i",Npar,entry), Form("h_%i_ws_data_%i",Npar,entry), 100, x_min, x_max);
+    TH1D* h_mc = new TH1D(Form("h_%i_mc_%i_%i",Npar,entry,isWrong), Form("h_%i_mc_%i_%i",Npar,entry,isWrong), nbins, x_min, x_max);
+    TH1D* h_rs_data = new TH1D(Form("h_%i_rs_data_%i_%i",Npar,entry,isWrong), Form("h_%i_rs_data_%i_%i",Npar,entry,isWrong), nbins, x_min, x_max);
+    TH1D* h_ws_data = new TH1D(Form("h_%i_ws_data_%i_%i",Npar,entry,isWrong), Form("h_%i_ws_data_%i_%i",Npar,entry,isWrong), nbins, x_min, x_max);
+    TH1D* h1_mc = new TH1D(Form("h1_%i_mc_%i_%i",Npar,entry,isWrong), Form("h1_%i_mc_%i_%i",Npar,entry,isWrong), nbins, x_min, x_max);
+    TH1D* h1_rs_data = new TH1D(Form("h1_%i_rs_data_%i_%i",Npar,entry,isWrong), Form("h1_%i_rs_data_%i_%i",Npar,entry,isWrong), nbins, x_min, x_max);
 
-    t1_2016->Draw(name+Form(" >> h_%i_mc_%i",Npar,entry)); 
-    t2_2016->Draw(name+Form(" >> h_%i_rs_data_%i",Npar,entry));
-    t3_2016->Draw(name+Form(" >> h_%i_ws_data_%i",Npar,entry));
+    t1->Draw(name+Form(" >> h_%i_mc_%i_%i",Npar,entry,isWrong)); 
+    t2->Draw(name+Form(" >> h_%i_rs_data_%i_%i",Npar,entry,isWrong));
+    t1->Draw(name+Form(" >> h1_%i_mc_%i_%i",Npar,entry,isWrong), mass_vetoe_cuts); 
+    t2->Draw(name+Form(" >> h1_%i_rs_data_%i_%i",Npar,entry,isWrong), mass_vetoe_cuts);
+    // t3->Draw(name+Form(" >> h_%i_ws_data_%i",Npar,entry));
 
     h_mc->SetLineColor(kBlue);
     h_rs_data->SetLineColor(kBlack);
     h_ws_data->SetLineColor(kRed);
+    h1_mc->SetLineColor(kAzure+1);
+    h1_rs_data->SetLineColor(kGreen+1);
 
     h_mc->GetXaxis()->SetTitle(name+" (MeV)");
-    h_mc->GetYaxis()->SetTitle(Form( "Normalized entries / (%.2f MeV)", (x_max - x_min)/100.  ));
-    h_mc->SetTitle("");
+    h_mc->GetYaxis()->SetTitle(Form( "Normalized entries / (%.2f MeV)", (x_max - x_min)/nbins  ));
 
     h_rs_data->GetXaxis()->SetTitle(name+" (MeV)");
-    h_rs_data->GetYaxis()->SetTitle(Form( "Normalized entries / (%.2f MeV)", (x_max - x_min)/100.  ));
-    h_rs_data->SetTitle("");
+    h_rs_data->GetYaxis()->SetTitle(Form( "Normalized entries / (%.2f MeV)", (x_max - x_min)/nbins  ));
 
-    h_ws_data->GetXaxis()->SetTitle(name+" (MeV)");
-    h_ws_data->GetYaxis()->SetTitle(Form( "Normalized entries / (%.2f MeV)", (x_max - x_min)/100.  ));
-    h_ws_data->SetTitle("");
+    h1_mc->GetXaxis()->SetTitle(name+" (MeV)");
+    h1_mc->GetYaxis()->SetTitle(Form( "Normalized entries / (%.2f MeV)", (x_max - x_min)/nbins  ));
 
-     if( (h_rs_data->GetMaximum() > h_mc->GetMaximum()) && (h_rs_data->GetMaximum() > h_ws_data->GetMaximum()) )
+    h1_rs_data->GetXaxis()->SetTitle(name+" (MeV)");
+    h1_rs_data->GetYaxis()->SetTitle(Form( "Normalized entries / (%.2f MeV)", (x_max - x_min)/nbins  ));
+
+    // h_ws_data->GetXaxis()->SetTitle(name+" (MeV)");
+    // h_ws_data->GetYaxis()->SetTitle(Form( "Normalized entries / (%.2f MeV)", (x_max - x_min)/nbins  ));
+
+    if(isWrong)
+    {
+        h_mc->SetTitle("Wrong mass hypothesis");
+        h_rs_data->SetTitle("Wrong mass hyopthesis");
+        h1_mc->SetTitle("Wrong mass hypothesis");
+        h1_rs_data->SetTitle("Wrong mass hyopthesis");
+    }
+    else
+    {
+        h_mc->SetTitle("Correct mass hypothesis");
+        h_rs_data->SetTitle("Correct mass hyopthesis");
+        h1_mc->SetTitle("Correct mass hypothesis");
+        h1_rs_data->SetTitle("Correct mass hyopthesis");
+    }
+
+    h_mc->Scale(1./h_mc->Integral());
+    h_rs_data->Scale(1./h_rs_data->Integral());
+    h1_mc->Scale(1./h1_mc->Integral());
+    h1_rs_data->Scale(1./h1_rs_data->Integral());
+    // h_ws_data->Scale(1./h_ws_data->Integral());
+
+    if( (h_rs_data->GetMaximum() > h_mc->GetMaximum()) && (h_rs_data->GetMaximum() > h1_mc->GetMaximum()) &&  (h_rs_data->GetMaximum() > h1_rs_data->GetMaximum()) )
     {
         h_mc->GetYaxis()->SetRangeUser(0, 1.1*(h_rs_data->GetMaximum()));
     }
-    else if( (h_ws_data->GetMaximum() > h_mc->GetMaximum()) && (h_ws_data->GetMaximum() > h_rs_data->GetMaximum()) )
+    else if( (h1_rs_data->GetMaximum() > h_mc->GetMaximum()) && (h1_rs_data->GetMaximum() > h1_mc->GetMaximum()) &&  (h1_rs_data->GetMaximum() > h_rs_data->GetMaximum()) )
     {
-        h_mc->GetYaxis()->SetRangeUser(0, 1.1*(h_ws_data->GetMaximum()));
+        h_mc->GetYaxis()->SetRangeUser(0, 1.1*(h1_rs_data->GetMaximum()));
+    }
+    else if( (h1_mc->GetMaximum() > h_mc->GetMaximum()) && (h1_mc->GetMaximum() > h_rs_data->GetMaximum()) &&  (h1_mc->GetMaximum() > h1_rs_data->GetMaximum()) )
+    {
+        h_mc->GetYaxis()->SetRangeUser(0, 1.1*(h1_mc->GetMaximum()));
     }
 
-    h_mc->DrawNormalized();
-    h_rs_data->DrawNormalized("same");
-    h_ws_data->DrawNormalized("same");
+    h_mc->Draw("HIST");
+    h_rs_data->Draw("HIST same");
+    h1_mc->Draw("HIST same");
+    h1_rs_data->Draw("HIST same");
+    // h_ws_data->Draw("HIST same");
 
     TLegend* leg = new TLegend(0.7, 0.7, 0.9, 0.9);
     leg->AddEntry(h_mc, "MC", "lp");
     leg->AddEntry(h_rs_data, "RS data", "lp");
-    leg->AddEntry(h_ws_data, "WS data", "lp");
+    leg->AddEntry(h1_mc, "MC (after cut)", "lp");
+    leg->AddEntry(h1_rs_data, "RS data (after cut)", "lp");
+    // leg->AddEntry(h_ws_data, "WS data", "lp");
     leg->Draw("same");
 
     c.SaveAs(Form("/panfs/felician/B2Ktautau/workflow/mass_vetoes_plots/%i_particles/",Npar)+name+".pdf");
 }
+
 
 Int_t n_choose_k(Int_t n, Int_t k)
 {
