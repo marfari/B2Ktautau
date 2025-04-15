4d39bdbb8b386f90a08a5758b29fe06fe3867a73
Feliciano Faria Maria Carolina <felician@lphesrv1.epfl.ch>
2025-04-15 09:54:49 +0200

    update

--- Final_fit/generate_toy_data.py
+++ Final_fit/generate_toy_data.py
@@ -24,10 +24,10 @@ def main(argv):
     f_bkg = ROOT.TFile("/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/Species_3/post_sel_tree_bdt1_0_bdt2_0.root")
     t_bkg = f_bkg.Get("DecayTree")
 
-    f_sig = ROOT.TFile("/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/Species_10/post_sel_tree_bdt1_0_bdt2_0.root")
-    t_sig = f_sig.Get("DecayTree")
+    # Different binning schemes
+    # f_sig = ROOT.TFile("/panfs/felician/B2Ktautau/workflow/create_post_selection_tree/Species_10/post_sel_tree_bdt1_0_bdt2_0.root")
+    # t_sig = f_sig.Get("DecayTree")
 
-    # nbins = 40
     # if( (t_sig.GetEntries("(BDT1 > {0}) && (BDT2 > {1})".format(bdt1,bdt2)) < 500) or (t_bkg.GetEntries("(BDT1 > {0}) && (BDT2 > {1})".format(bdt1,bdt2)) < 500) ):
     #     nbins = 10
     # if( (t_sig.GetEntries("(BDT1 > {0}) && (BDT2 > {1})".format(bdt1,bdt2)) < 5000) or (t_bkg.GetEntries("(BDT1 > {0}) && (BDT2 > {1})".format(bdt1,bdt2)) < 5000) ):
@@ -43,40 +43,47 @@ def main(argv):
     #     bins =  array.array('d', [4000, 4250, 4500, 4700, 4900, 5100, 5300, 5500, 5700, 5900, 6100, 6300, 6500, 6700, 6900, 7450, 8000 ])
     # if((bdt1 > 0.99) or (bdt2 > 0.99)): # 4000
     #     bins =  array.array('d', [4000, 4400, 4800, 5200, 5600, 6000, 6400, 7000, 8000])
+
+    # bins = array.array('d', [4000, 4500, 4600, 4700, 4800, 4900, 5000, 5100, 5200, 5300, 5400, 5500, 5600, 5700, 5800, 5900, 6000, 6100, 6200, 6300, 6400, 6500, 7250, 8000])
+    
+    # bins = array.array('d', [4000, 4500, 4650, 4800, 4950, 5100, 5250, 5400, 5550, 5700, 5850, 6000, 6500, 8000])
     # nbins = len(bins) - 1
 
     # Compute optimal number of bins using the Freedman-Diaconis rule
-    nbins = 40
-    if((bdt1 >= 0.5) and (bdt2 >= 0.5)):
-        h_data_norm = ROOT.TH1D("h_data_norm", "h_data_norm", nbins, 4000, 8000)
-        t_bkg.Draw("df_Bp_M >> h_data_norm", "(BDT1 > {0}) && (BDT2 > {1})".format(bdt1,bdt2))
+    # nbins = 40
+    # if((bdt1 >= 0.5) and (bdt2 >= 0.5)):
+    #     h_data_norm = ROOT.TH1D("h_data_norm", "h_data_norm", nbins, 4000, 8000)
+    #     t_bkg.Draw("df_Bp_M >> h_data_norm", "(BDT1 > {0}) && (BDT2 > {1})".format(bdt1,bdt2))
 
-        quantiles = np.array([0.0, 0.0])
-        prob = np.array([0.25, 0.75])
-        h_data_norm.GetQuantiles(2, quantiles, prob)
+    #     quantiles = np.array([0.0, 0.0])
+    #     prob = np.array([0.25, 0.75])
+    #     h_data_norm.GetQuantiles(2, quantiles, prob)
 
-        q1, q3 = quantiles[0], quantiles[1]
-        iqr = q3-q1
+    #     q1, q3 = quantiles[0], quantiles[1]
+    #     iqr = q3-q1
 
-        N = h_data_norm.GetEntries()
-        if(N == 0):
-            bin_width = 4000
-        else:
-            bin_width = 2*iqr/(N**(1/3))
+    #     N = h_data_norm.GetEntries()
+    #     if(N == 0):
+    #         bin_width = 4000
+    #     else:
+    #         bin_width = 2*iqr/(N**(1/3))
 
-        nbins = int((8000-4000)/bin_width)
-        print(bin_width)
+    #     nbins = int((8000-4000)/bin_width)
+    #     print(bin_width)
+
+    # 1) Create histogram (WS data B+ mass)
+    nbins = 40 # 100 MeV bin width (~half of signal mass resolution)
 
     # Use optimal number of bins
     h_data = ROOT.TH1D("h_data", "h_data", nbins, 4000, 8000)
     t_bkg.Draw("df_Bp_M >> h_data", "(BDT1 > {0}) && (BDT2 > {1})".format(bdt1,bdt2))
 
+    # 2) Sample from WS data histogram
     eps_ws_den = t_bkg.GetEntries()
     eps_ws_num = t_bkg.GetEntries("(BDT1 > {0}) && (BDT2 > {1})".format(bdt1,bdt2))
     eps_ws = eps_ws_num/eps_ws_den
-
     N_bkg = int(2*19006409*eps_ws)
-
+    
     np.random.seed(random_seed)
     N = np.random.poisson(N_bkg)
 
